b160e2abf591d2d6b2b1015f0dbf458f3a8602d4
frletmecook540-spec <frletmecook540@gmail.com>
2025-11-01 18:48:18 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+
+import qiskit
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+def main():
+    print("System ready")
+
+if __name__ == "__main__":
+    main()
